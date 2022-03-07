e1b1cc9bb52c4b80e994fc41353b7bd5911f0d51
Leeky0615 <rbdus7174@naver.com>
2022-03-07 17:44:38 +0900

    feat: add toRefs example

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,10 +1,20 @@
 <template>
   <div>Home Page</div>
+  <div>{{ count }}</div>
+  <button @click="count++">ADD</button>
 </template>
 
 <script>
-export default {
+import {useCount} from "../composables/count";
 
+export default {
+  setup() {
+    const {count} = useCount();
+    console.log(count);
+    return {
+      count
+    }
+  }
 }
 </script>
 
